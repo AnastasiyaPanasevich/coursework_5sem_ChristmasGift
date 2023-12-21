10ab8df9e0de0f1b2de4db947bb4351955a7e573
Anastasiya Panasevich <matildaweller66@gmail.com>
2023-12-21 09:00:19 +0300

    добавлен примитивный обмен между сервером и клиентом

--- ChristmasGiftClient/Model/Client.cs
+++ ChristmasGiftClient/Model/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ChristmasGiftClient.Model
 {
@@ -9,25 +10,32 @@ namespace ChristmasGiftClient.Model
         private static string serverIP = "127.0.0.1";
         private static int serverPort = 8888;
 
-        static void Main()
+        public static async Task RunClientAsync(string message)
         {
             TcpClient client = new TcpClient(serverIP, serverPort);
 
             try
             {
                 NetworkStream stream = client.GetStream();
-                string message = "example";
                 byte[] data = Encoding.ASCII.GetBytes(message);
 
                 stream.Write(data, 0, data.Length);
                 Console.WriteLine($"Sent message to server: {message}");
 
-                // Чтение ответа от сервера
-                byte[] responseData = new byte[1024];
-                int bytesRead = stream.Read(responseData, 0, responseData.Length);
-                string responseMessage = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                // Чтение ответа от сервера в отдельном потоке с тайм-аутом
+                Task<string> readTask = Task.Run(() => ReadFromStream(stream));
 
-                Console.WriteLine($"Received response from server: {responseMessage}");
+                if (await Task.WhenAny(readTask, Task.Delay(5000)) == readTask)
+                {
+                    // Ответ получен в течение 5 секунд
+                    string responseMessage = await readTask;
+                    Console.WriteLine($"Received response from server: {responseMessage}");
+                }
+                else
+                {
+                    // Тайм-аут
+                    Console.WriteLine("Server did not respond within 5 seconds.");
+                }
             }
             catch (Exception ex)
             {
@@ -37,8 +45,13 @@ namespace ChristmasGiftClient.Model
             {
                 client.Close();
             }
+        }
 
-            Console.ReadLine();
+        private static async Task<string> ReadFromStream(NetworkStream stream)
+        {
+            byte[] responseData = new byte[1024];
+            int bytesRead = await stream.ReadAsync(responseData, 0, responseData.Length);
+            return Encoding.ASCII.GetString(responseData, 0, bytesRead);
         }
     }
 }
